d713012428efd38558cdedf2ac801471a8a07226
U-hamza <umair_hamza@hotmail.co.uk>
2026-04-26 12:08:59 +0100

    Flask + Redis App

---
+++ flask-redis-app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import redis
+import os
+
+app = Flask(__name__)
+
+# Connect to Redis (service name = "redis")
+redis_host = os.environ.get("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379)
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask + Redis App!"
+
+@app.route("/count")
+def count():
+    visits = r.incr("counter")
+    return f"Visit count: {visits}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
